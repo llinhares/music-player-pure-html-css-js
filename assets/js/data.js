908d9bdbe505ef0bf29e0c2d3c6968da30bd3f24
llinhares <lucas.lteccorp@gmail.com>
2021-09-12 00:00:48 -0300

    build mobile first front-end

---
+++ assets/js/data.js
@@ -0,0 +1,62 @@
+let songs = [
+  {
+      name: 'Flight of the Inner Bird',
+      path: 'assets/musics/flight-of-the-inner-bird_by_sivan-talmor.mp3',
+      artist: 'Sivan Talmor',
+      cover: 'assets/imgs/Sivan-Talmor-Vol-2-.jpg'
+  },
+  {
+      name: 'Coming Back Home',
+      path: 'assets/musics/coming-back-home_by_wearethegood.mp3',
+      artist: 'WEARETHEGOOD',
+      cover: 'assets/imgs/WEARETHEGOOD-Reckless-Summer.jpg'
+  },
+  {
+      name: 'Urban Rebel',
+      path: 'assets/musics/urban-rebel_by_ikoliks.mp3',
+      artist: 'ikoliks',
+      cover: 'assets/imgs/ikoliks-Urban-Rebel.jpg'
+  },
+  {
+      name: 'song 4',
+      path: 'assets/musics/Song 4.mp3',
+      artist: 'artist 4',
+      cover: 'assets/imgs/cover 4.png'
+  },
+  {
+      name: 'song 5',
+      path: 'assets/musics/Song 5.mp3',
+      artist: 'artist 5',
+      cover: 'assets/imgs/cover 5.png'
+  },
+  {
+      name: 'song 6',
+      path: 'assets/musics/Song 6.mp3',
+      artist: 'artist 6',
+      cover: 'assets/imgs/cover 6.png'
+  },
+  {
+      name: 'song 7',
+      path: 'assets/musics/Song 7.mp3',
+      artist: 'artist 7',
+      cover: 'assets/imgs/cover 7.png'
+  },
+  {
+      name: 'song 8',
+      path: 'assets/musics/Song 8.mp3',
+      artist: 'artist 8',
+      cover: 'assets/imgs/cover 8.png'
+  },
+  {
+      name: 'song 9',
+      path: 'assets/musics/Song 9.mp3',
+      artist: 'artist 9',
+      cover: 'assets/imgs/cover 9.png'
+  },
+  {
+      name: 'song 10',
+      path: 'assets/musics/Song 10.mp3',
+      artist: 'artist 10',
+      cover: 'assets/imgs/cover 10.png'
+  }
+]
